0765e498df89472ac01bc8c36708a43694108720
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-08 10:04:47 +0200

    use Load Balancer-backed to call License Service

--- src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
+++ src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
@@ -1,40 +1,27 @@
 package com.mycompany.secondmicroservice.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InvokeLicenseService {
 
-    private final DiscoveryClient discoveryClient;
-
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     private String serviceId = "firstmicroservice";
 
     public String invoke() {
+        String licenseServiceUrl = "http://" + serviceId + "/";
 
-        List<ServiceInstance> instanceInfos = discoveryClient.getInstances(serviceId);
-
-        if (! instanceInfos.isEmpty()) {
-
-            String licenseServiceUrl = instanceInfos.get(0).getUri().toString();
-
-            return restTemplate.exchange(
-                    licenseServiceUrl,
-                    HttpMethod.GET,
-                    null,
-                    String.class
-            ).getBody();
-        }
-
-        return null;
+        return restTemplate.exchange(
+                licenseServiceUrl,
+                HttpMethod.GET,
+                null,
+                String.class
+        ).getBody();
     }
+
 }
