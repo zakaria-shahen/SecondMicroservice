3d6a9a857b6516e14512f8995307ccac1f130dd4
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-11 14:45:46 +0200

    use resilience4j @Bulkhead, @RateLimiter, @Retry in InvokeLicenseService

--- src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
+++ src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
@@ -1,14 +1,23 @@
 package com.mycompany.secondmicroservice.service;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("firstmicroservice")
+@FeignClient(InvokeLicenseService.SERVICE_NAME)
 public interface InvokeLicenseService {
 
+     String SERVICE_NAME = "firstMicroservice";
+     String FALLBACK_METHOD = "firstMicroserviceFallback";
 
-    @CircuitBreaker(name = "firstMicroservice", fallbackMethod = "firstMicroserviceFallback")
+    //  default: type = Bulkhead.Type.SEMAPHORE
+    @Bulkhead(name = SERVICE_NAME, fallbackMethod = FALLBACK_METHOD, type = Bulkhead.Type.SEMAPHORE)
+    @RateLimiter(name = SERVICE_NAME, fallbackMethod = FALLBACK_METHOD)
+    @CircuitBreaker(name = SERVICE_NAME, fallbackMethod = FALLBACK_METHOD)
+    @Retry(name = SERVICE_NAME, fallbackMethod = FALLBACK_METHOD)
     @GetMapping
     String invoke();
 
