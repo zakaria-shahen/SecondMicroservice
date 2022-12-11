739a65a072d443f3bc0e42db08f6b50ef2466bdb
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-11 14:00:24 +0200

    use resilience4j @CircuitBreaker in InvokeLicenseService

--- src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
+++ src/main/java/com/mycompany/secondmicroservice/service/InvokeLicenseService.java
@@ -1,12 +1,18 @@
 package com.mycompany.secondmicroservice.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient("firstmicroservice")
 public interface InvokeLicenseService {
 
+
+    @CircuitBreaker(name = "firstMicroservice", fallbackMethod = "firstMicroserviceFallback")
     @GetMapping
     String invoke();
 
+    default String firstMicroserviceFallback(Throwable throwable) {
+        return "firstMicroservice: Not available";
+    }
 }
